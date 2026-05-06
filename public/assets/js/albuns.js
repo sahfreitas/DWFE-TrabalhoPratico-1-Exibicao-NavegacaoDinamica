621866d1ef125f24e576b8dc8e6d122c5f4cd7ca
Sara da Cruz Freitas <saradacruzfreitas@gmail.com>
2026-05-06 17:19:48 -0300

    js

---
+++ public/assets/js/albuns.js
@@ -0,0 +1,119 @@
+const itens = {
+    "albuns": [
+        {
+            "id": 1,
+            "nome": "Emails I Can’t Send",
+            "artista": "Sabrina Carpenter",
+            "descricao": "Álbum introspectivo com letras pessoais e sonoridade pop madura.",
+            "conteudo": "Lançado em 2022, o álbum explora temas como relacionamentos, amadurecimento e autoconhecimento, com uma abordagem mais vulnerável e direta nas letras.",
+            "genero": "Pop",
+            "destaque": true,
+            "data": "2022-07-15",
+            "imagem_principal": "img/emails_i_cant_send.jpg",
+            "faixas": [
+                {
+                    "id": 1,
+                    "nome": "emails i can’t send",
+                    "duracao": "1:44"
+                },
+                {
+                    "id": 2,
+                    "nome": "Vicious",
+                    "duracao": "2:30"
+                },
+                {
+                    "id": 3,
+                    "nome": "because i liked a boy",
+                    "duracao": "3:16"
+                },
+                {
+                    "id": 4,
+                    "nome": "Nonsense",
+                    "duracao": "2:43"
+                },
+                {
+                    "id": 5,
+                    "nome": "Fast Times",
+                    "duracao": "2:54"
+                }
+            ]
+
+        },
+        {
+            "id": 2,
+            "nome": "Did You Know That There’s a Tunnel Under Ocean Blvd",
+            "artista": "Lana Del Rey",
+            "descricao": "Álbum melancólico e poético com forte carga emocional e produção sofisticada.",
+            "conteudo": "Lançado em 2023, o álbum traz reflexões sobre identidade, família e vulnerabilidade, mantendo o estilo cinematográfico característico de Lana Del Rey.",
+            "genero": "Alternative / Pop",
+            "destaque": true,
+            "data": "2023-03-24",
+            "imagem_principal": "img/ocean_blvd.jpg",
+            "faixas": [
+                {
+                    "id": 1,
+                    "nome": "The Grants",
+                    "duracao": "4:55"
+                },
+                {
+                    "id": 2,
+                    "nome": "Did You Know That There’s a Tunnel Under Ocean Blvd",
+                    "duracao": "4:45"
+                },
+                {
+                    "id": 3,
+                    "nome": "A&W",
+                    "duracao": "7:13"
+                },
+                {
+                    "id": 4,
+                    "nome": "Paris, Texas",
+                    "duracao": "3:26"
+                },
+                {
+                    "id": 5,
+                    "nome": "Margaret",
+                    "duracao": "5:39"
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "nome": "One in a Million",
+            "artista": "Aaliyah",
+            "descricao": "Álbum marcante do R&B com produção inovadora e influente.",
+            "conteudo": "Lançado em 1996, o álbum consolidou Aaliyah como um dos maiores nomes do R&B, trazendo uma sonoridade moderna com produção de Timbaland e Missy Elliott.",
+            "genero": "R&B",
+            "destaque": false,
+            "data": "1996-08-27",
+            "imagem_principal": "",
+            "faixas": [
+                {
+                    "id": 1,
+                    "nome": "One in a Million",
+                    "duracao": "4:30"
+                },
+                {
+                    "id": 2,
+                    "nome": "If Your Girl Only Knew",
+                    "duracao": "4:51"
+                },
+                {
+                    "id": 3,
+                    "nome": "Hot Like Fire",
+                    "duracao": "4:23"
+                },
+                {
+                    "id": 4,
+                    "nome": "4 Page Letter",
+                    "duracao": "4:38"
+                },
+                {
+                    "id": 5,
+                    "nome": "Choosey Lover (Old School / New School)",
+                    "duracao": "3:59"
+                }
+            ]
+        }
+    ]
+}
